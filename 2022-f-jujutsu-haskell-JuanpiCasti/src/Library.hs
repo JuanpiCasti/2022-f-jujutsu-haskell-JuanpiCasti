594827b3f83f5ead633f90a7efad96a406018dbf
JuanpiCasti <juanpablocastiglione01@gmail.com>
2022-04-21 19:50:36 -0300

    Agregado ejercicio 11

--- 2022-f-jujutsu-haskell-JuanpiCasti/src/Library.hs
+++ 2022-f-jujutsu-haskell-JuanpiCasti/src/Library.hs
@@ -5,7 +5,7 @@ doble :: Number -> Number
 doble n = n * 2
 
 -----------------------
--- Definiciones de tipos
+-- 1
 data Hechicero = Hechicero {
     nombre :: String,
     antiguedad :: Number,
@@ -15,22 +15,20 @@ data Hechicero = Hechicero {
 
 type Grupo = [Hechicero]
 
--- Experiencia
+-- 2
 minimoExperiencia :: Number
 minimoExperiencia = 1
 tieneExperiencia :: Hechicero -> Bool
 tieneExperiencia hechicero = antiguedad hechicero > minimoExperiencia
 
-soloExpertos :: Grupo -> Grupo
-soloExpertos grupo = filter tieneExperiencia grupo
 
--- Preparacion
+-- 3
 minimoPreparado :: Number
 minimoPreparado = 3
 estanPreparados :: Grupo -> Bool
 estanPreparados grupo = (length grupo) > minimoPreparado
 
--- Relacionado a grado
+-- 4
 gradoEspecial :: Number
 gradoEspecial = 0
 esGradoEspecial :: Hechicero -> Bool
@@ -39,30 +37,44 @@ subirDeGrado :: Hechicero -> Hechicero
 subirDeGrado hechicero | esGradoEspecial hechicero = hechicero
                        | otherwise = (Hechicero (nombre hechicero) (antiguedad hechicero) (clan hechicero) ((grado hechicero) - 1))
 
-sonInvencibles :: Grupo -> Bool
-sonInvencibles grupo = any esGradoEspecial grupo
-
-powerUp :: Grupo -> Grupo
-powerUp grupo = map subirDeGrado grupo
-
-soloEspeciales :: Grupo -> Grupo
-soloEspeciales grupo = filter esGradoEspecial grupo
-
-cantidadEspeciales :: Grupo -> Number
-cantidadEspeciales grupo = length (soloEspeciales grupo)
--- Relacionado a clanes
+-- 5
 clanesPrestigiosos :: [String]
 clanesPrestigiosos = ["Zening", "Gojo", "Kamo"]
 esPrestigioso :: Hechicero -> Bool
 esPrestigioso hechicero = elem (clan hechicero) clanesPrestigiosos
 
+-- 6
+sonInvencibles :: Grupo -> Bool
+sonInvencibles grupo = any esGradoEspecial grupo
+
+-- 7
 sonFavoritos :: Grupo -> Bool
 sonFavoritos grupo = all esPrestigioso grupo
 
+-- 8
+soloExpertos :: Grupo -> Grupo
+soloExpertos grupo = filter tieneExperiencia grupo
 
--- 9
+-- 9 a
 haceFrente :: Grupo -> Bool
 haceFrente grupo = sonInvencibles grupo || estanPreparados grupo
+
+-- 9 b
+powerUp :: Grupo -> Grupo
+powerUp grupo = map subirDeGrado grupo
+
+
+-- 10
+soloEspeciales :: Grupo -> Grupo
+soloEspeciales grupo = filter esGradoEspecial grupo
+
+cantidadEspeciales :: Grupo -> Number
+cantidadEspeciales grupo = length (soloEspeciales grupo)
+
+-- 11
+promedioGrados :: Grupo -> Number
+promedioGrados grupo = sum(map grado grupo) / length grupo
+
 
 -- Data de prueba
 pepito = Hechicero "Pepito" 1 "Pepe" 0
